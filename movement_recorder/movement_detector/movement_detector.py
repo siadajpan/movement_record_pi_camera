5e9f06b4768d9461316d03845567cd2d2f1f682d
Karol Misiarz <karol.misiarz@cervirobotics.com>
2020-03-29 11:22:35 +0200

    Update movement detector to save count of history files

--- movement_recorder/movement_detector/movement_detector.py
+++ movement_recorder/movement_detector/movement_detector.py
@@ -6,22 +6,35 @@ from movement_recorder import settings
 
 class MovementDetector:
     def __init__(self):
-        # self.movement_boxes: List[Rectangle] = []
         self.background_subtractor = cv2.createBackgroundSubtractorKNN(
             history=settings.PreProcessing.HISTORY,
             dist2Threshold=settings.PreProcessing.DIST_TO_THRESHOLD,
             detectShadows=settings.PreProcessing.DETECT_SHADOWS
         )
-        # self.kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (5, 5))
+        self.history_count = 0
+        self.full_history_count = settings.PreProcessing.HISTORY
 
-    def check_mean_threshold(self, binary_image):
+    def _check_mean_threshold(self, binary_image):
         mean_above_threshold = np.mean(binary_image) \
                                > settings.PreProcessing.MOVEMENT_MEAN_THRESHOLD
 
         return mean_above_threshold
 
+    def reset_history_count(self):
+        self.history_count = 0
+
+    def _check_history_count(self):
+        if self.history_count <= self.full_history_count:
+            self.history_count += 1
+            return False
+        else:
+            return True
+
     def analyze_image(self, image: np.array):
         foreground = self.background_subtractor.apply(image)
-        movement = self.check_mean_threshold(foreground)
+
+        movement = False
+        if self._check_history_count():
+            movement = self._check_mean_threshold(foreground)
 
         return movement, foreground
