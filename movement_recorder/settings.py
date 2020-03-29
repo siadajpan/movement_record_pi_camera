1cbc83f588f868ae31d2d8968f6e2dbf5712df3e
Karol Misiarz <siadajpan@o2.pl>
2020-03-29 10:41:30 +0100

    change settings for faster acquisition

--- movement_recorder/settings.py
+++ movement_recorder/settings.py
@@ -1,5 +1,5 @@
 class Camera:
-    MOVEMENT_FPS = 5
+    MOVEMENT_FPS = 10
     MOVEMENT_RESOLUTION = (640, 480)
     RECORD_FPS = 20
     RECORD_RESOLUTION = (1920, 1080)
@@ -8,7 +8,7 @@ class Camera:
 
 
 class PreProcessing:
-    HISTORY = 100
+    HISTORY = 40
     DIST_TO_THRESHOLD = 300
     DETECT_SHADOWS = False
     MOVEMENT_MEAN_THRESHOLD = 10
