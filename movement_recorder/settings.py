a694aee8f8919b560c8cb83eefc16ba430b4ee81
Karol Misiarz <karol.misiarz@cervirobotics.com>
2020-03-29 10:16:07 +0200

    Add settings for movement detector

--- movement_recorder/settings.py
+++ movement_recorder/settings.py
@@ -4,3 +4,10 @@ class Camera:
     RECORD_FPS = 20
     RECORD_RESOLUTION = (1920, 1080)
     RECORD_EXTENSION = 'mjpeg'
+
+
+class PreProcessing:
+    HISTORY = 100
+    DIST_TO_THRESHOLD = 300
+    DETECT_SHADOWS = False
+    MOVEMENT_MEAN_THRESHOLD = 10
